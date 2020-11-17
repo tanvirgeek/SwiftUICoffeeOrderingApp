3e5da59c153fd192c9e531f1afb611136306b3e2
tanvir <tanvirgeek@gmail.com>
2020-11-17 06:57:46 +0600

    Initial Commit

---
+++ SwiftUICoffeeOrderingApp/SwiftUICoffeeOrderingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoffeeOrderingAppApp.swift
+//  SwiftUICoffeeOrderingApp
+//
+//  Created by MD Tanvir Alam on 17/11/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoffeeOrderingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
